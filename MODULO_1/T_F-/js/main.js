c8ea653822b510e65129fd39ccbca4bfa0e438e6
himnie <almorabitibrahim@gmail.com>
2025-03-03 19:38:45 +0100

    actua-03-03-2025

---
+++ MODULO_1/T_F-/js/main.js
@@ -0,0 +1,89 @@
+      // funcion para calcular la edad del usuario
+    
+
+    const formulario = document.querySelector("#form");
+
+    const aside = document.querySelector("#barraLateral");
+
+    formulario.onsubmit = function (e) {
+        e.preventDefault()
+        let nombreUsuario = document.querySelector("#nombreUsuario").value;
+        let fechaNacimiento = document.querySelector("#fechaNacimiento").value;
+        let estudios = document.querySelector("#estudios").value;
+        // creamos un objeto date con la fecha de nacimiento
+        const fechaNacimientoUsuario = new Date(fechaNacimiento);
+
+        // obtenemos la fecha actual del sistema
+        const fechaActual = new Date();
+
+        // aseguramos que la fecha de nacimiento no sea futura
+        if (fechaNacimientoUsuario > fechaActual) {
+            alert("no puedes ser del futuro, amigo !");
+            return;
+        }
+
+        // calculamos la diferencia de años entre las fechas
+        let edad = fechaActual.getFullYear() - fechaNacimientoUsuario.getFullYear();
+
+        // ajustamos la edad si todavia no ha sido tu cumpleaños este año
+      
+        const mesNacimiento = fechaNacimientoUsuario.getMonth();
+        const diaNacimiento = fechaNacimientoUsuario.getDate();
+        const mesActual = fechaActual.getMonth();
+        const diaActual = fechaActual.getDate();
+
+        if (mesActual < mesNacimiento || (mesActual === mesNacimiento && diaActual < diaNacimiento)) {
+            edad--; // restamos un año porque todavia no cumples años
+        } if (edad < 18) {
+            alert('debes ser mayor de edad !');
+        }
+        else {
+            aside.innerHTML = `
+            <h2>
+                Datos de registro:
+
+            </h2>
+            Nombre: ${nombreUsuario}<br>
+            Fecha de Nacimiento: ${fechaNacimiento}<br>
+            Estudios: ${estudios}<br>
+            Edad: ${edad}<br>
+
+
+                           `
+        }
+           //desaparece primer form
+           formulario.style.display = "none";
+           //mostramos contenedor de tareas
+           document.getElementById("divcontenedor").style.display = "block";
+    }
+
+      
+     
+
+     //obtenemos los objetos creados en html con el dom
+     let inputTarea = document.getElementById('inputTarea');
+     const anadirTareaBtn = document.querySelector('#añadirTareaBtn');
+     let lista = document.querySelector('#listaTareas');
+
+     console.log(inputTarea.value);
+     console.log(lista.value);
+
+
+     function anadirTareas() {
+         let tarea = inputTarea.value;
+
+         console.log(tarea);
+         if (!tarea || tarea == '') { return; }
+         let elemenLista = `
+ <li>${tarea}
+ <button class="borrarBtn" onclick=" this.parentNode.remove()">
+     <span style="color: red; font-weight: bold;">&times;</span>
+
+ </button>
+ </li>`;
+         lista.innerHTML += elemenLista;
+         inputTarea.value = '';
+
+     }
+     anadirTareaBtn.onclick = anadirTareas;
+
